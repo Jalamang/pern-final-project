7acc9dfa35130c4cdcf1e4d4049e74c1fe3d41fe
Jalamang <Jalamangjanka@gmail.com>
2022-02-22 22:39:30 -0500

    Heroku deployment

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,6 +1,15 @@
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS electronics;
 
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY, 
-    name TEXT
+CREATE TABLE electronics (
+    productid SERIAL PRIMARY KEY, 
+    name TEXT,
+    picture TEXT,
+    price NUMERIC,
+    capacity NUMERIC,
+    description TEXT,
+    material TEXT,
+    rating INT,
+    CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN DEFAULT true
 );
+
