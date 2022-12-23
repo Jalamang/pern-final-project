11badc6d238e5ffefb4b5352dbd9122cca7c4fb7
Jalamang Janka <42457441+Jalamang@users.noreply.github.com>
2022-12-23 18:00:16 -0500

    Update server.js

--- back-end/server.js
+++ back-end/server.js
@@ -3,13 +3,13 @@ const app = require("./app.js");
 
 // CONFIGURATION
 require("dotenv").config();
-let {PORT} = require("../back-end/constant/index")
+// let {PORT} = require("../back-end/constant/index")
 
 
-PORT = process.env.PORT || 5000;
+// PORT = process.env.PORT || 5000;
 
 // LISTEN
-app.listen(PORT, () => {
-  console.log(`🎧 Listening on port ${PORT} 🎧 `);
+app.listen(5000, () => {
+  console.log(`🎧 Listening on port ${5000} 🎧 `);
 });
 
