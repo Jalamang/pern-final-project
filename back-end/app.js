bd172d16c454c97faa1362e0b62b928c11fc2bd6
Jalamang Janka <42457441+Jalamang@users.noreply.github.com>
2022-12-23 17:28:17 -0500

    Update app.js

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { CLIENT_URL } = require("./constant");
 const passport = require("passport");
 
 
